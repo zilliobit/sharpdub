88d4eeeafada25b31a527b98d653a9ff863dbf29
zilliobit <46642683+zilliobit@users.noreply.github.com>
2026-03-12 14:17:10 +0530

    Delete backend directory

--- backend/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from apps.auth.routes import router as auth_router
-from apps.todos.routes import router as todos_router
-
-
-api_router = APIRouter()
-api_router.include_router(auth_router)
-api_router.include_router(todos_router)
--- backend/db/base.py
+++
@@ -1,6 +0,0 @@
-from db.database import Base 
-# Import your models so they "register" themselves onto that Base
-from apps.auth.models import User, Role, UserRole, BackupCode
-
-from apps.todos.models import Todo
-
